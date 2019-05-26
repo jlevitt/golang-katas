d37613cb087ed5b0706f85779f27c559458268fb
Jake Levitt <jlevitt@vt.edu>
2019-05-26 15:10:39 -0400

    Better stack.

--- polish/stack/stack.go
+++ polish/stack/stack.go
@@ -2,7 +2,6 @@ package stack
 
 type stack struct {
 	values []int
-	size   int
 }
 
 func NewStack() stack {
@@ -10,22 +9,20 @@ func NewStack() stack {
 }
 
 func (s *stack) Push(value int) {
-	s.size++
 	s.values = append(s.values, value)
 }
 
 func (s *stack) Pop() (int, bool) {
-	if s.size == 0 {
+	if len(s.values) == 0 {
 		return 0, false
 	}
 
-	value := s.values[s.size-1]
-	s.values = s.values[:s.size-1]
-	s.size--
+	value := s.values[len(s.values)-1]
+	s.values = s.values[:len(s.values)-1]
 
 	return value, true
 }
 
 func (s *stack) Size() int {
-	return s.size
+	return len(s.values)
 }
